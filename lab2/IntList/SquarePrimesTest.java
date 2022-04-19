285b957e7633b6a8e00ee93dfed886ce16993870
Purkinje-cell <2100012256@stu.pku.edu.cn>
2022-04-19 14:58:38 +0800

    done with IntList

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,18 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimeHarder() {
+        IntList lst = IntList.of(2,3,5,6,7,11,97);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 6 -> 49 -> 121 -> 9409",lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimeFalse() {
+        IntList lst = IntList.of(4,6,8,10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 8 -> 10",lst.toString());
+        assertFalse(changed);
+    }
 }
